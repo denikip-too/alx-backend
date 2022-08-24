e8f6637a63210027745fffd7cc54700414c1961d
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-24 21:18:17 +0000

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""inherits from BaseCaching and is a caching system"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """subclass FIFOCache"""
+
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value for the
+        key key"""
+        if key or item in self.cache_data is None:
+            pass
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            for key, item in list(self.cache_data.items()):
+                del self.cache_data[key]
+                print("DISCARD: {}".format(key))
+                break
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        if key not in self.cache_data or key is None:
+            return (None)
+        return (self.cache_data[key])
