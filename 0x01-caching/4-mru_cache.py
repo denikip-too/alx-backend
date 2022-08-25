bd658f5dbb2684af98e524715aa6c24840869265
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-25 01:05:02 +0000

    ...

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""inherits from BaseCaching and is a caching system"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """subclass LIFOCache"""
+
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value for the
+        key key"""
+        if key or item in self.cache_data is None:
+            pass
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            for key, item in self.cache_data.items():
+                print("DISCARD: {}".format(key))
+                break
+            self.cache_data.popitem(last=False)
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        if key not in self.cache_data or key is None:
+            return (None)
+        self.cache_data.move_to_end(key)
+        return (self.cache_data[key])
