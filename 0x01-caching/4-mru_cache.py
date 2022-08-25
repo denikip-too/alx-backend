d1bf00c0d7f1ab4469af21fdd5593da01cf6ed73
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-25 01:06:36 +0000

    ...

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     """subclass LIFOCache"""
 
     def __init__(self):
