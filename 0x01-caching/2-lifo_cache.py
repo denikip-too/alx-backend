fb1bb6d2470501cd7463fda79ef21360b4d365d3
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-26 23:54:00 +0000

    LIFO

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -14,7 +14,7 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item value for the
         key key"""
-        if key is not None and item is not None and len(self.cache_data) <= 4:
+        if key is not None and item is not None:
             self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             for key, item in list(self.cache_data.items()):
