2d284a34d178a76d53427b152f8503b10e31d352
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-26 23:59:11 +0000

    MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -15,15 +15,14 @@ class MRUCache(BaseCaching):
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item value for the
         key key"""
-        if key or item in self.cache_data is None:
-            pass
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key)
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            for key, item in self.cache_data.items():
-                print("DISCARD: {}".format(key))
+            for key, item in list(self.cache_data.items()):
+                self.cache_data.popitem(last=False)
+                print("DISCARD: {}".format(list(self.cache_data.keys())[-1]))
                 break
-            self.cache_data.popitem(last=False)
 
     def get(self, key):
         """return the value in self.cache_data linked to key"""
