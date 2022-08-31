79e8851b37996f70d8ccbf9587e02c7f3b9503fc
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-31 12:30:41 +0000

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,39 +2,23 @@
 """Basic Babel setup"""
 from flask_babel import Babel
 from pytz import timezone, UTC
-from babel import Locale
+from flask import Flask, render_template
 app = Flask(__name__)
-
-
 babel = Babel(app)
 
 
-class config(object):
+class Config(object):
     """configure available languages in our app"""
 
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE ='UTC'
 
-    def __init__(self, default_locale='en', default_timezone='UTC'):
-        """set Babel’s default locale ("en") and timezone ("UTC")"""
-        self._default_locale = default_locale
-        self._default_timezone = default_timezone
 
-    @property
-    def default_timezone(self):
-        """The default timezone from the configuration as instance of a
-        `pytz.timezone` object.
-        """
-        return timezone(self.app.config['BABEL_DEFAULT_TIMEZONE'])
+app.config.from_object('Config')
 
-    @property
-    def default_locale(self):
-        """The default locale from the configuration as instance of a
-        `babel.Locale` object.
-        """
-        return Locale.parse(self.app.config['BABEL_DEFAULT_LOCALE'])
 
-    @app.route('/')
-    def local_time():
-        """display local time"""
-        return (render_template(
-            '1-index.html', utc_dt=self.default_timezone()))
+@app.route('/'
+def home():
+    """return simple outputs"""
+    return render_template('1-index.html')
