06945ea7acecceed58477b422636608704eba4dd
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-03 01:45:27 +0000

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Basic Babel setup"""
+from flask_babel import Babel
+from pytz import timezone, UTC
+from flask import Flask, request, render_template
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    """configure available languages in our app"""
+
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    """return simple outputs"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return (locale)
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """returns a user dictionary"""
+
+
+@app.before_request
+def before_request():
+    """Define a before_request"""
+    flask.g.user = get_user()
