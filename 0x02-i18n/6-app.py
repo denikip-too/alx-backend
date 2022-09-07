cd9b63e78d837c81183946ac8932239805f14788
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-07 22:26:07 +0000

    Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -19,6 +19,14 @@ class Config(object):
 app.config.from_object(Config)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 @app.route('/', methods=['GET'])
 def home():
     """return simple outputs"""
